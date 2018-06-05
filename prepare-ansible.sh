fac499fc6ca811acc3fb629f2b11648fd8296f6a
bstachmann <egal>
2018-06-05 14:43:46 +0200

    Eliminate prepare-Script. Move installation of git and ansible to run-Script.

--- prepare-ansible.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install ansible
-sudo apt install git
-ansible-galaxy install -r requirements.yml --roles-path=~/ansible-roles "$@"
--- run-ansible.sh
+++ run-ansible.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+if ! command -v ansible > /dev/null ; then
+  sudo apt update
+
+  sudo apt install -y ansible
+
+  if ! command -v git > /dev/null ; then
+    sudo apt install -y git
+  fi
+
+  ansible-galaxy install -r requirements.yml --roles-path=~/ansible-roles "$@"
+fi
+
 ansible-playbook ubuntu-dev.yml --ask-sudo-pass "$@"
